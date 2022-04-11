243aad33e20b5e385851f8ddb349aa978d011919
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2022-04-11 10:44:13 +0200

    inscription

--- authentification/src/components/Inscription.js
+++ authentification/src/components/Inscription.js
@@ -1,63 +1,116 @@
-import React from "react";
 import { Form, Field } from "react-final-form";
-
-export default function Connection() {
+import axios from "axios";
+import React, { useContext, useRef, useState } from "react";
+import "./Inscription.css";
+export default function Inscription() {
   const handleSubmit = () => {};
-  const onSubmit = () => {};
+  // const onSubmit = () => {};
   const validate = () => {};
   const InterestPicker = () => {};
-    const MyForm = () => (
+  const [connecter, setConnecter] = useState(false);
+  const [validation, setValidation] = useState("");
+
+  const createuser = async ({ email, name, password }) => {
+    const res = await axios.put("http://localhost:3001/user", {
+      email: email,
+      name: name,
+      password: password,
+    });
+    return res;
+  };
+
+  const onSubmit = (values) => {
+    console.log("prout", values);
+    let res;
+    res = createuser(values);
+    console.log(res, "res");
+    if (res == 500) {
+      setValidation("erreur");
+    } else {
+      setConnecter(true);
+    }
+  };
+
+  function verif(values) {
+    console.log("prout2", values);
+    if (values.password != values.password2) {
+      setValidation("mdp différent");
+    } else {
+      onSubmit(values);
+    }
+  }
+
+  const MyForm = () => (
     <Form
-      onSubmit={onSubmit}
+      onSubmit={verif}
       validate={validate}
       render={({ handleSubmit }) => (
         <form onSubmit={handleSubmit}>
-          <h2>Simple Default Input</h2>
-          <div>
-            <label>Pseudo</label>
-            <Field name="firstName" component="input" placeholder="First Name" />
-          </div>
-
-          <h2>An Arbitrary Reusable Input Component</h2>
-          <div>
-            <label>mot de passe</label>
-            <Field name="interests" component={InterestPicker} />
-          </div>
-
-          <h2>Render Function</h2>
           <Field
-            name="bio"
+            name="email"
             render={({ input, meta }) => (
               <div>
-                <label>Bio</label>
+                <label>email</label>
                 <textarea {...input} />
                 {meta.touched && meta.error && <span>{meta.error}</span>}
               </div>
             )}
           />
 
-          <h2>Render Function as Children</h2>
-          <Field name="phone">
+          {/* <h2>Render Function</h2> */}
+          <Field
+            name="name"
+            render={({ input, meta }) => (
+              <div>
+                <label>name</label>
+                <textarea {...input} />
+                {meta.touched && meta.error && <span>{meta.error}</span>}
+              </div>
+            )}
+          />
+
+          {/* <h2>Render Function as Children</h2> */}
+          <Field name="password">
             {({ input, meta }) => (
               <div>
-                <label>Phone</label>
-                <input type="text" {...input} placeholder="Phone" />
+                <label>password</label>
+                <input type="password" {...input} placeholder="password" />
                 {meta.touched && meta.error && <span>{meta.error}</span>}
               </div>
             )}
           </Field>
 
+          <Field name="password2">
+            {({ input, meta }) => (
+              <div>
+                <label>confirm password</label>
+                <input type="password" {...input} placeholder="password2" />
+                {meta.touched && meta.error && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <div>
+            <p className="inscription-validation">{validation}</p>
+          </div>
+
           <button type="submit">Submit</button>
         </form>
       )}
     />
-  )
- 
+  );
 
   return (
     <>
-      <h1>Inscription </h1>
-      <MyForm></MyForm>
+      {connecter ? (
+        <>
+          <h1>Vous vous êtes inscrits</h1>
+        </>
+      ) : (
+        <>
+          <h1>Inscription </h1>
+          <MyForm></MyForm>
+        </>
+      )}
     </>
   );
 }
